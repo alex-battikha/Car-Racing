5a4e68d3e054d969b87853e4ea95c420ae0ddccd
alex-battikha <56514843+alex-battikha@users.noreply.github.com>
2021-05-17 00:38:14 -0700

    initial commit; game completed

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Car-Racing
-The final project for the Car-Racing game
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ js/Player.js
@@ -0,0 +1,49 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  getRank() {
+    var playerRankNumber = database.ref('rankNumber');
+    playerRankNumber.on("value", (data)=>{
+      this.rank = data.val();
+    });
+  }
+
+  updateRank(rank) {
+    database.ref('/').update({
+      rankNumber: rank
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
